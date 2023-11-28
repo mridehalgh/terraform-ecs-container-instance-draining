1dea1d20ef6f475ebeda7117a930eb81df942b13
Justin Lyons <jlyons@lexile.com>
2023-11-28 17:25:17 -0500

    Upgrade to 3.11

--- iam.tf
+++ iam.tf
@@ -69,14 +69,14 @@ data "aws_iam_policy_document" "lambda" {
 }
 
 resource "aws_iam_role" "lambda" {
-  name               = format("%.41s-draining-function-role", regex("[[:alnum:]]+",var.autoscaling_group_name))
+  name               = format("%.41s-draining-function-role", regex("[[:alnum:]]+", var.autoscaling_group_name))
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
 
   tags = var.tags
 }
 
 resource "aws_iam_role_policy" "lambda_execution_policy" {
-  name = format("%.39s-draining-function-policy", regex("[[:alnum:]]+",var.autoscaling_group_name))
+  name = format("%.39s-draining-function-policy", regex("[[:alnum:]]+", var.autoscaling_group_name))
   role = aws_iam_role.lambda.id
 
   policy = data.aws_iam_policy_document.lambda.json
@@ -100,7 +100,7 @@ data "aws_iam_policy_document" "lifecycle_assume_role" {
 }
 
 resource "aws_iam_role" "lifecycle" {
-  name               = format("%.49s-lifecycle-role", regex("[[:alnum:]]+",var.autoscaling_group_name))
+  name               = format("%.49s-lifecycle-role", regex("[[:alnum:]]+", var.autoscaling_group_name))
   assume_role_policy = data.aws_iam_policy_document.lifecycle_assume_role.json
 
   tags = var.tags
@@ -120,7 +120,7 @@ data "aws_iam_policy_document" "lifecycle_policy" {
 }
 
 resource "aws_iam_role_policy" "lifecycle_execution_policy" {
-  name = format("%.47s-lifecycle-policy", regex("[[:alnum:]]+",var.autoscaling_group_name))
+  name = format("%.47s-lifecycle-policy", regex("[[:alnum:]]+", var.autoscaling_group_name))
   role = aws_iam_role.lifecycle.id
 
   policy = data.aws_iam_policy_document.lifecycle_policy.json
